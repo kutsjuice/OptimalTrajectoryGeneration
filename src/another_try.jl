ca28a826198b787bf07554affd85d7a5f2243e5a
zasyp45 <zasypkin4514@gmail.com>
2026-02-05 15:21:32 +0300

    Wrote a small example, still debugging

--- src/another_try.jl
+++ src/another_try.jl
@@ -42,8 +42,8 @@ unskew(s::SMatrix{3,3,Float64}) = SVector(s[3,2], s[1,3], s[2,1])
 
 # cross motion product operator
 function crm(v::SVector{6,Float64})
-    w = v[1:3]
-    v_lin = v[4:6]
+    w = SVector{3,Float64}(v[1:3]...)
+    v_lin = SVector{3,Float64}(v[4:6]...)
     return @SMatrix [
         skew(w)          zeros(3,3);
         skew(v_lin)     skew(w)
@@ -55,10 +55,12 @@ function Xmotion(
     E::SMatrix{3,3,Float64},
     r::SVector{3,Float64}   
 )
-    @SMatrix [
-    E               zeros(3,3);
-    -skew(r*E)          E
-    ]
+    P = -skew(r) * E    
+    X = zeros(6,6)
+    X[1:3, 1:3] = E
+    X[4:6, 1:3] = P
+    X[4:6, 4:6] = E
+    return SMatrix{6,6,Float64}(X)
 end
 
 function to_plucker(T::SMatrix{4,4})
@@ -94,7 +96,7 @@ function jcalc(
         XJ = Xmotion(E, @SVector zeros(3))
         S = @SVector [0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
     elseif pitch == Inf
-        XJ = Xmotion(@SMatrix{3,3,Float64}(I), @SVector [0.0, 0.0, q])
+        XJ = Xmotion(SMatrix{3,3,Float64}(I), @SVector [0.0, 0.0, q])        
         S = @SVector [0.0, 0.0, 0.0, 0.0, 0.0, 1.0]
     else
         E = @SMatrix[
@@ -122,26 +124,26 @@ function inverse_dynamics(
     S   = Vector{SVector{6,Float64}}(undef, n)
     Xup = Vector{SMatrix{6,6,Float64}}(undef, n)
 
-    a0 = @SVector [0.0; 0.0; 0.0; -robot.gravity[1]; -robot.gravity[2]; -robot.gravity[3]]
+    a0 = @SVector [0.0; 0.0; 0.0; -model.gravity[1]; -model.gravity[2]; -model.gravity[3]]
     τ = zeros(n)
     for i in 1:n
-        XJ, S[i] = jcalc(robot.links[i].pitch, q[i])
-        Xup[i] = robot.links[i].Xtree * XJ
-        if robot.links[i].parent == 0
+        XJ, S[i] = jcalc(model.links[i].pitch, q[i])
+        Xup[i] = model.links[i].Xtree * XJ
+        if model.links[i].parent == 0
             vJ = S[i] * qd[i]
             v[i] = vJ
             a[i] = Xup[i] * a0 + S[i] * qdd[i] + crm(v[i]) * vJ
         else
-            j = robot.links[i].parent
+            j = model.links[i].parent
             vJ = S[i] * qd[i]
             v[i] = Xup[i] * v[j] + vJ
             a[i] = Xup[i] * a[j] + S[i] * qdd[i] + crm(v[i]) * vJ
         end
     end
     for i in n:-1:1
-        f[i] = robot.links[i].I * a[i] + crm(v[i])' * (robot.links[i].I * v[i])
-        if robot.links[i].parent != 0
-            j = robot.links[i].parent
+        f[i] = model.links[i].I * a[i] + crm(v[i])' * (model.links[i].I * v[i])
+        if model.links[i].parent != 0
+            j = model.links[i].parent
             f[j] += Xup[i]' * f[i]
         end
          τ[i] = S[i]' * f[i]
@@ -221,3 +223,76 @@ function inverse_kinematics(model::Model, target_pose::SMatrix{4,4,Float64}, ini
     end
     q
 end
+
+#EXAMPLE USAGE
+
+inertia1 = Inertia(
+    1.0,
+    diagm(@SVector [0.01, 0.01, 0.02]),
+    @SVector [0.15, 0.0, 0.0]
+)
+
+inertia2 = Inertia(
+    0.8,
+    diagm(@SVector [0.008, 0.008, 0.015]),
+    @SVector [0.125, 0.0, 0.0]
+)
+
+inertia3 = Inertia(
+    0.5,
+    diagm(@SVector [0.002, 0.002, 0.001]),
+    @SVector [0.0, 0.0, -0.05]
+)
+
+link1 = Link(
+    0,
+    0.0,
+    Xmotion(SMatrix{3,3,Float64}(I), @SVector [0.0, 0.0, 0.0]),
+    inertia1
+)
+
+link2 = Link(
+    1,
+    0.0,
+    Xmotion(SMatrix{3,3,Float64}(I), @SVector [0.3, 0.0, 0.0]),
+    inertia2
+)
+
+link3 = Link(
+    2,
+    Inf,
+    Xmotion(SMatrix{3,3,Float64}(I), @SVector [0.25, 0.0, 0.0]),
+    inertia3
+)
+
+scara_robot = Model(
+    3,
+    [link1, link2, link3],
+    @SVector [0.0, 0.0, 9.81]
+)
+
+println("SCARA робот создан:")
+println("Количество суставов: ", dof(scara_robot))
+println("Гравитация: ", scara_robot.gravity)
+
+q = [0.5, 0.3, 0.1]
+qd = [0.1, 0.2, 0.01]
+qdd = [0.01, 0.02, 0.001]
+
+τ = inverse_dynamics(scara_robot, q, qd, qdd)
+
+println("\nУсилия на суставах:")
+println("Сустав 1 (вращательный): τ = ", τ[1], " Н·м")
+println("Сустав 2 (вращательный): τ = ", τ[2], " Н·м") 
+println("Сустав 3 (поступательный): f = ", τ[3], " Н")
+
+X_ee = forward_kinematics(scara_robot, q, 3)
+println("\nПоза энд-эффектора (матрица 6x6):")
+println(X_ee)
+
+position = X_ee[1:3, 4]
+println("Позиция энд-эффектора (x, y, z): ", position)
+
+J = bodyJac(scara_robot, 3, q)
+println("\nЯкобиан энд-эффектора (6x3):")
+println(J)
